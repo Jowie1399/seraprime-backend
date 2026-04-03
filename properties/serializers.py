bf860083541b69fc149cc98a873404290ab66aa2
jowie1399 <werejoe94@gmail.com>
2026-04-03 16:31:44 +0300

    backend update

--- properties/serializers.py
+++ properties/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Property, Unit, Tenant, Lease
+ from datetime import date
+from calendar import monthrange
 
 
 class PropertySerializer(serializers.ModelSerializer):
@@ -23,6 +25,10 @@ class TenantSerializer(serializers.ModelSerializer):
 
     unit_name = serializers.SerializerMethodField()
     unit_id = serializers.SerializerMethodField()
+    rent_amount = serializers.SerializerMethodField()   
+    start_date = serializers.SerializerMethodField()  
+    next_due_date = serializers.SerializerMethodField()
+  
 
     class Meta:
         model = Tenant
@@ -42,6 +48,39 @@ class TenantSerializer(serializers.ModelSerializer):
     def get_rent_amount(self, obj):  #  ADDED THIS
         lease = obj.leases.filter(is_active=True).first()
         return str(lease.rent_amount) if lease else None
+    
+        from datetime import date
+    from calendar import monthrange
+
+    def get_next_due_date(self, obj):
+        lease = obj.leases.filter(is_active=True).first()
+
+        if not lease:
+            return None
+
+        today = date.today()
+
+        # current month due date
+        last_day = monthrange(today.year, today.month)[1]
+        due_day = min(lease.due_day, last_day)
+
+        current_due = date(today.year, today.month, due_day)
+
+        # if already passed → next month
+        if current_due < today:
+            if today.month == 12:
+                year = today.year + 1
+                month = 1
+            else:
+                year = today.year
+                month = today.month + 1
+
+            last_day = monthrange(year, month)[1]
+            due_day = min(lease.due_day, last_day)
+
+            return date(year, month, due_day)
+
+        return current_due
 
 class LeaseSerializer(serializers.ModelSerializer):
     tenant_name = serializers.CharField(source="tenant.full_name", read_only=True)
